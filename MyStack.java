9d8133b5da9d6512e8595a37960492aa1fa9d551
siramirezar-afk <siramirezar@unal.edu.co>
2026-03-23 15:25:58 -0500

    Implement stack push, pop, top, and resize methods

--- MyStack.java
+++ MyStack.java
@@ -14,24 +14,35 @@ public class MyStack<T> {
 
     // Inserta elemento en el tope
     public void push(T value) {
-        // TODO
+        if (size == capacity) {
+            resize(capacity * 2);
+        }
+        data[size] = value;
+        size++;
     }
 
     // Elimina el elemento del tope
     public void pop() {
-        // TODO
+        if (empty()) {
+            System.out.println("Error: Stack vacío");
+            return;
+        }
+        data[size -1] = null;
+        size--;
     }
 
     // Retorna el elemento del tope
     public T top() {
-        // TODO
-        return null;
+        if (empty()) {
+            System.out.println("Error: Stack vacío");
+            return null;
+        }
+        return data[size - 1];
     }
 
     // Verifica si está vacío
     public boolean empty() {
-        // TODO
-        return false;
+        return size == 0;
     }
 
     // Retorna el tamaño actual
@@ -44,13 +55,21 @@ public class MyStack<T> {
         return capacity;
     }
 
-    // Redimensiona el arreglo (por ejemplo, duplicando capacidad)
+    // Redimensiona el arreglo (duplica la capacidad)
     private void resize(int newCapacity) {
-        // TODO
+        T[] newData = (T[]) new Object[newCapacity];
+        for (int i = 0; i< size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+        capacity = newCapacity;
     }
 
     // Limpia el stack
     public void clear() {
-        // TODO
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        size = 0;
     }
 }
