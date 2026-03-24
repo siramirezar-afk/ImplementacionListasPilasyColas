058c73b4edb401d194f9b01f68d8114886366968
siramirezar-afk <siramirezar@unal.edu.co>
2026-03-23 19:22:25 -0500

    Add stack operations to MyStack class
    Implemented push, pop, top, empty, resize, and clear methods for stack functionality.

--- MyStack.java
+++ MyStack.java
@@ -1,9 +1,7 @@
 public class MyStack<T> {
-
     private T[] data;
     private int size;
     private int capacity;
-
     // Constructor
     @SuppressWarnings("unchecked")
     public MyStack() {
@@ -14,43 +12,61 @@ public class MyStack<T> {
 
     // Inserta elemento en el tope
     public void push(T value) {
-        // TODO
+        if (size == capacity) {
+            resize(capacity * 2);
+        }
+        data[size] = value;
+        size++;
     }
 
     // Elimina el elemento del tope
     public void pop() {
-        // TODO
+        if (empty()) {
+            System.out.println("Error: Stack vacío");
+            return;
+        }
+        data[size -1] = null;
+        size--;
     }
 
     // Retorna el elemento del tope
     public T top() {
-        // TODO
-        return null;
+        if (empty()) {
+            System.out.println("Error: Stack vacío");
+            return null;
+        }
+        return data[size - 1];
     }
 
     // Verifica si está vacío
     public boolean empty() {
-        // TODO
-        return false;
+        return size == 0;
     }
 
     // Retorna el tamaño actual
     public int size() {
         return size;
     }
-
     // Retorna la capacidad actual
     public int capacity() {
         return capacity;
     }
 
-    // Redimensiona el arreglo (por ejemplo, duplicando capacidad)
+    // Redimensiona el arreglo (duplica la capacidad)
     private void resize(int newCapacity) {
-        // TODO
+        T[] newData = (T[]) new Object[newCapacity];
+        for (int i = 0; i< size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+        capacity = newCapacity;
     }
 
     // Limpia el stack
     public void clear() {
-        // TODO
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        size = 0;
     }
 }
